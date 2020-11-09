5263bd6689e7c07e6be4c653ed75b5b3b1db48e8
Lara Vomfell <l.vomfell@warwick.ac.uk>
2020-11-09 14:48:06 +0000

    add some comments

--- utils.R
+++ utils.R
@@ -5,33 +5,42 @@
 source('inpoly.r')
 library(fields)
 
-simpson <- function(value, dx=1)
-{
-    m <- length(value)
-    
+# pretty self explanatory
+dist.squared <- function(x1, y1, x2, y2) {(x1-x2)^2+(y1-y2)^2}
 
-    bx <- c(1, rep(c(4,2), (m-3)/2), 4, 1)
-
-    sum(value*bx)*dx/3.0
+# this is Simpson's integral approximation where you approx
+# int_a^b f(x)dx by splitting [a,b] into m chunks and then calcul
+# (b-a)/2m * (f(a) + 4f(x1) + 2f(x2) + ..+ 4(f(x_m-1) + f(b))
+# This function already takes f(a), f(x1) etc and then just 
+# calculates the final result
+# dx gives the chunk size
+simpson <- function(value, dx = 1){
+  # how many chunks were given
+  m <- length(value)
+  
+  # set up the chunk multipliers
+  bx <- c(1, rep(c(4, 2), (m - 3)/2), 4, 1)
+  
+  sum(value * bx) * dx/3.0
 }
 
-
-simpson.2D <- function(value, dx=1, dy=1)
-{
+# same procedure, just now with double integral
+# as before, value is already the function evaluated (eg f(x1))
+# at the chunks; this function just puts everything together
+simpson.2D <- function(value, dx=1, dy=1){
     m <- nrow(value)
     n <- ncol(value)
     
-    bx <- c(1, rep(c(4,2), (m-3)/2), 4, 1)
-    by <- c(1, rep(c(4,2), (n-3)/2), 4, 1)
+    bx <- c(1, rep(c(4, 2), (m - 3)/2), 4, 1)
+    by <- c(1, rep(c(4, 2), (n - 3)/2), 4, 1)
     
-    sum( (bx%*%value%*%by)*dx*dy/9.0)   
+    sum((bx %*% value %*% by) * dx * dy/9.0)   
 }
 
-ker.smooth.conv<- function(x, y, bandwidth)
-{
+ker.smooth.conv <- function(x, y, bandwidth){
   yy <- c(y,rev(y))
 
-  n<- length(x)
+  n <- length(x)
   dx <- x[2]-x[1]
   xx <- ((1:n)*dx-dx/2) 
   
@@ -43,15 +52,16 @@ ker.smooth.conv<- function(x, y, bandwidth)
   (smoothed[1:(n+1)])
 }
 
-# this function 
-## euqla weights
-
-hist.weighted <- function(x, weight, breaks)
-   {
+# this function takes in a vector x and a break vector breaks
+# and then essentially just calculates a histogram
+# there are two tricks: they apply weights to midpoints (which they add just in case we don't observe them)
+# prior to all this they also define 'tempfun' which smoothly interpolates
+# between x and the breaks
+hist.weighted <- function(x, weight, breaks)   {
        try(if(min(x) < min(breaks) | max(x) > max(breaks)) stop("'breaks' do not span range of 'x'") )
        nb <- length(breaks)
        
-       # I have no idea what this does.
+       # smooth interpolation between breaks and the length of breaks (0:200 eg)
        tempfun <- stepfun(breaks, 0:nb)
        # calculate midpoints between breaks (breaks = 0.005, ...s etc)
        mids <- (breaks[2:nb]+breaks[1:(nb-1)])*.5
@@ -59,17 +69,18 @@ hist.weighted <- function(x, weight, breaks)
        x <- c(x, mids)
        # assign zero weight to those points
        weight <- c(weight, rep(0, nb-1))
-       ## what does this do??s
+       # get value of smooth interpolation at x
        z <- tempfun(x)
        # sum weights at each unique z (equal to table basically)
        freq <- tapply(weight, z, sum)
-       list(breaks=breaks, counts=freq, density=freq/sum(freq)/(breaks[2:nb]-breaks[1:(nb-1)]) , mids=mids)
-   }
+       list(breaks=breaks, 
+            counts=freq, 
+            density=freq/sum(freq)/(breaks[2:nb]-breaks[1:(nb-1)]), 
+            mids=mids)
+}
 
-hist.weighted.1D <- hist.weighted
 
-hist.weighted.2D <- function(x, y, weight, x.breaks, y.breaks)
-{
+hist.weighted.2D <- function(x, y, weight, x.breaks, y.breaks){
    try(if(min(x) < min(x.breaks) | max(x) > max(x.breaks) | min(y) < min(y.breaks)
           | max(y) > max(y.breaks) ) stop("'breaks' do not span range of 'x' or 'y'!") )   
    nbx <- length(x.breaks)
@@ -117,10 +128,7 @@ ker.smooth.fft<- function(x, z, bandwidth){
   Re(smoothed[1:(nx+1)])
 }
 
-ker.smooth.1D.fft <- ker.smooth.fft
-
-ker.smooth.2D.fft<- function(x, y, z, x.bandwidth,y.bandwidth)
-{
+ker.smooth.2D.fft<- function(x, y, z, x.bandwidth,y.bandwidth){
   
   nx <- nrow(z)
   ny <- ncol(z)
