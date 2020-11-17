b13757b9bcf7908a931525e3e53f0fbfe207c5e1
Lara Vomfell <l.vomfell@warwick.ac.uk>
2020-11-17 11:13:22 +0000

    moved necessary libraries and functions from others files to file actually using it

--- prepare_data.R
+++ prepare_data.R
@@ -1,5 +1,15 @@
 library(spatstat)
+library(sf)
+library(polyCub)
+library(mvtnorm)
+gpclibPermit()
 
+library(foreach)
+library(doParallel)
+
+no_cores <- parallel::detectCores()
+cl <- makeCluster(no_cores)
+registerDoParallel(cl)
 # THIS CAN BE SKIPPED IF type1_crime_datatable already exists
 
 # Read Crime data
@@ -10,9 +20,9 @@ crimes$minute <- substr(crimes$time,4,5)
 crimes$second <- substr(crimes$time,7,8)
 
 crimes$days <-(julian(as.Date(crimes$date), orig=as.Date('2012-1-1'))
-      +as.double(crimes$hour)/24
-      +as.double(crimes$minute)/24/60
-      +as.double(crimes$second)/24/60/60 )
+               +as.double(crimes$hour)/24
+               +as.double(crimes$minute)/24/60
+               +as.double(crimes$second)/24/60/60 )
 
 # crime type == 1
 a<-crimes[crimes[,4]==1,]
@@ -28,14 +38,14 @@ a$coory = a$coory /1000+runif(nrow(a), -0.0005, 0.0005)
 a$bandwidth <- rep(0.005,nrow(a))
 
 for(i in 1:nrow(a)){
-  # calculate sq distances to all points not == i
+   # calculate sq distances to all points not == i
    temp <- dist.squared(a$coorx[i], a$coory[i], a$coorx[-i], a$coory[-i])
    # keep fifth smallest distance above threshold???
    # maybe something like we need at least four points nearby??
    temp2 <- sqrt(sort(temp[temp >= 0.002^2])[5])
    # replace bandwidth if too small otherwise
    if(a$bandwidth[i] <= temp2){
-     a$bandwidth[i] = temp2
+      a$bandwidth[i] = temp2
    }
 }
 
@@ -56,12 +66,12 @@ a$bg.integral <- rep(0, nrow(a))
 w= owin(c(749, 755), c(4428, 4433), poly=city.boundary)
 
 for(i in 1:nrow(a)){
-  # calculate exact integral
-  a$bg.integral[i] <- polyCub.exact.Gauss(w,
-                                          mean=c(a$coorx[i], a$coory[i]),
-                                          Sigma=diag(a$bandwidth[i], 2), plot=F)
-  print(paste(i, 'of', nrow(a),":", a$bg.integral[i]))
-
+   # calculate exact integral
+   a$bg.integral[i] <- polyCub.exact.Gauss(w,
+                                           mean=c(a$coorx[i], a$coory[i]),
+                                           Sigma=diag(a$bandwidth[i], 2), plot=F)
+   print(paste(i, 'of', nrow(a),":", a$bg.integral[i]))
+   
 }
 
 write.table(a, file="type1_crime_data.table")
