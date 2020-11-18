68fb56f9ac80dc3cf151c93678e1c21f6a544ddd
Lara Vomfell <l.vomfell@warwick.ac.uk>
2020-11-18 18:21:04 +0000

    semi polished figure updates

--- fig3_plot_model_comp.r
+++ fig3_plot_model_comp.r
@@ -1,50 +1,115 @@
 library(ggplot2)
+library(viridis)
+library(osmdata)
+library(magrittr)
 
+theme_print <- function(...){
+  theme_light() +
+    theme(text = element_text(size = 14),
+          plot.background = element_blank(),
+          legend.background = element_blank(), 
+          legend.key = element_blank(),
+          strip.text = element_text(colour = 'black'),
+          strip.background = element_rect(fill = "lightgray")) +
+    theme(...)
+}
 
-# plotting mu_bg
+theme_set(theme_print())
 
-df_mu <- list(x = background_base$x[seq(1, length(background_base$x), 18)],
-              y = background_base$y[seq(1, length(background_base$y), 18)],
-              z = as.matrix(log10(background_basevalue * (background_marks)/TT/boundary_area))[seq(1, length(background_base$x), 18),
-                                                                                      seq(1, length(background_base$y), 18)])
+# plotting mu_bg and overlaying it with streets
+mu_bg <- cbind(expand.grid(x = background_base$x,
+                          y = background_base$y),
+               z = as.vector((background_basevalue * (background_marks)/TT/boundary_area)))
+# drop undefined values
+mu_bg <- mu_bg[mu_bg$z > 0,]
 
-dimnames(df_mu$z) <- list(x = df_mu$x, y = df_mu$y)
+# get streets
+street_values <- c("motorway",
+                   "trunk",
+                   "primary", "secondary", "tertiary")
+street_values <- c(street_values, paste0(street_values, "_link"),
+                    "unclassified", "residential", "living_street", "pedestrian")
 
-m2 = reshape2::melt(df_mu, value.name = "z")
-m2 = m2[!is.na(m2$x),]
-ggplot(m2, aes(x,y,z = z, fill = z)) + geom_tile() # or geom_raster(interpolate = TRUE)
+# define query for all roads
+roads <- opq(bbox = osm_bbox) %>%
+  add_osm_feature(key = "highway", value = street_values) %>%
+  osmdata_sf()
+roads <- roads$osm_lines
+
+# keep only the lines inside shp
+roads <- st_transform(roads, crs = 27700)
+roads <- st_intersection(roads, shp)
+
+# define line thinkness for streets
+roads$street_fill <- dplyr::case_when(roads$highway %in% c("residential", 
+                                                           "unclassified", 
+                                                           "tertiary", 
+                                                           "tertiary_link", 
+                                                           "living_street") ~ "B",
+                                      roads$highway == "pedestrian" ~ "C",
+                                      TRUE ~ "A")
+
+p <- ggplot() + 
+  # plot underlying background intensity
+  geom_tile(data = mu_bg, aes(x * 1000, y * 1000, fill = z)) + 
+  # plot colors on log10 scale
+  scale_fill_viridis_c(trans = scales::log10_trans(), direction = -1, option = "magma",
+                       breaks = c(0.000001, 0.00001, 0.01),
+                       labels = c(0.000001, 0.00001, 0.01)) + 
+  # add roads
+  geom_sf(data = roads, aes(size = street_fill), inherit.aes = F) +
+  scale_size_manual(values = c("A" = .75, "B" = .65, "C" = .4), guide = FALSE) +
+  coord_sf(datum = NA) +
+  labs(x = "", y = "")
+ggsave("figures/mu_bg.png", plot = p)
 
 # plotting mu_trend
-# I think this needs more
-trend <- data.frame(x = trend_base, y = trend_basevalue)
+trend <- data.frame(x = time_marks, y = trend_basevalue)
 
-ggplot(trend, aes(x,y)) + geom_line()
- par(fig=c(0, 1,0.7,1), mar=c(4,4,1.5,1), mgp=c(1.5,0.5,0), cex.lab=1.25,new=F)
-
- plot(trend.base, trend.basevalue,type="l", lwd=2, col=2, xlab="Time in days",
-       ylab=expression(mu[t](t)), main="(b) Trend")
-
+p <- ggplot(trend, aes(x,y)) + geom_line() +
+  scale_x_continuous(breaks = c(31, 213, 397),
+                     labels = c("01/Jan 2016", "01/Jul 2016", "01/Jan 2017")) +
+  scale_y_continuous(limits = c(0.4, 1.25)) +
+  labs(x = "", y = "mu trend")
+ggsave("figures/mu_trend.png", plot = p)
 
 # plotting mu_weekly
+# careful this depends on the data
 weekly <- data.frame(x = weekly_base, y = weekly_basevalue)
-ggplot(weekly, aes(x,y)) + geom_line()
+p <- ggplot(weekly, aes(x,y)) + geom_line() +
+  scale_x_continuous(breaks = seq(0, 6), labels = c("Tue", "Wed", "Thu", "Fri", "Sat", "Sun", "Mon")) +
+  labs(x = "Weekday", y = "mu weekly")
+ggsave("figures/mu_weekly.png", plot = p)
 
 # plotting mu_daily
 daily <- data.frame(x = daily_base, y = daily_basevalue)
-ggplot(daily, aes(x, y)) + geom_line()
+p <- ggplot(daily, aes(x, y)) + geom_line() +
+  scale_x_continuous(breaks = seq(0,1, .25), labels = c("0:00", "06:00", "12:00", "18:00", "0:00")) +
+  labs(x = "Time of day", y = "mu daily")
+ggsave("figures/mu_daily.png", plot = p)
 
 # plotting g(t)
 gt <- data.frame(x = g_base, y = g_basevalue)
-ggplot(gt, aes(x,y)) + geom_points(data = g_temp, aes(mids, density)) + geom_line()
+p <- ggplot(gt, aes(x,y)) + geom_point(data = data.frame(mids = g_temp$mids[-1],
+                                                    density = g_temp$density[-1]), 
+                                  aes(mids, density), alpha = .4, shape = 21, color = "black") + 
+  geom_line(color = "darkgreen", size = 0.9) +
+  labs(x = "Time in days", y = "Temporal excitation")
+ggsave("figures/gt.png", plot = p)
+
+
+p <- ggplot(gt, aes(x,y)) + geom_point(data = data.frame(mids = g_temp$mids[-1],
+                                                         density = g_temp$density[-1]), 
+                                       aes(mids, density), alpha = .4, shape = 21, color = "black") + 
+  geom_line(color = "darkgreen", size = 0.9) +
+  labs(x = "Time in days", y = "Temporal excitation") +
+  coord_cartesian(xlim = c(0, 3))
+ggsave("figures/gt_zoomed.png", plot = p)
 
 # plotting h(s)
-df_hs <- list(x = h_base_x,
-              y = h_base_y,
-              z = h_basevalue)
-dimnames(df_hs$z) = list(x = h_base_x, y = h_base_y)
+h_s <- cbind(expand.grid(x = h_base_x * 1000, y = h_base_y * 1000),
+            z = as.vector(h_basevalue))
 
-df_hs <- reshape2::melt(df_hs, value.name = "z")
-df_hs <- df_hs[!is.na(df_hs$x),]
-
-ggplot(df_hs, aes(x,y,z =z )) + geom_contour()
-
+p <- ggplot(h_s, aes(x,y,z =z )) + geom_contour(color = "black") +
+  labs(x = "Distance in m", y = "Distance in m")
+ggsave("figures/hs.png", plot = p)
--- fig4_cumul_events.r
+++ fig4_cumul_events.r
@@ -1,45 +1,34 @@
-
+no_cores <- parallel::detectCores()
+cl <- makeCluster(no_cores)
+registerDoParallel(cl)
 
 bg_at_all_locations <- trend_fun(time_marks) * weekly_fun(time_marks) * daily_fun(time_marks) * mean(background_fun(background_basex, 
                                                                                                                     background_basey) * background_marks) * ra
 
 
 trigger_at_all_locations <- foreach(i = 1:nrow(a)) %dopar% trigger_at_all_fun(i = i, constants = bg_weight * ra)
-# some simplification. I think reduce(.., `+`)
+trigger_at_all_locations <- reduce(trigger_at_all_locations, `+`)
 
 lambda_at_all_locations <- mu0 * bg_at_all_locations + A * trigger_at_all_locations
+save(lambda_at_all_locations, file = "results/lambda_at_all_locations.Rdata")
 
-plot(cumsum(lambda_at_all_locations) * space_units, stepfun(a$days, (0:nrow(a)))(time_marks))
 
-triggers.at.all.locations.no.A <- 0
+# creating fig 4(a)
+p <- ggplot(data.frame(x = c(0, a$days),
+                  y = 0:nrow(a)), aes(x,y)) +
+  geom_abline(intercept = 0, slope = nrow(a)/TT) +
+  geom_line(size = .8, color = "darkgreen")
+ggsave("figures/org_time.png", plot = p)
 
-for(i in 1:nrow(a)){ 
-           if(as.integer(i/100)*100==i)print(i)
-        
-           temp <- excite.spatial.fun(background.basex-a$coorx[i], 
-                                               background.basey-a$coory[i])
-    
-           triggers.at.all.locations.no.A <- (triggers.at.all.locations.no.A+ (excite.temporal.fun(time.marks-a$days[i]))*
-      mean((background.marks>0)*temp)* (Xrange[2]-Xrange[1])*(Yrange[2]-Yrange[1]))
-       }
-
-  lambda.at.all.locations <-  mu* bgrates.at.all.locations.no.mu + A * triggers.at.all.locations.no.A
-
-  cumsum(lambda_at_events) * space_units
- 
-postscript("raw-residual.ps", paper="special", height=7, width=14)
-
- par(mfrow=c(1,2),cex=1.2)
-
-  plot(c(0,a$days), 0:nrow(a), type="s",xlab="Time (days)", ylab="Sum. Freq.", lwd=2, xaxs="i", yaxs="i",xlim=c(0,735), ylim=c(0, 5200),main="(a)",cex.main=1.5)
-  abline(0, nrow(a)/730,lwd=1.5, lty=2)   
-
-  plot( cumsum(lambda.at.all.locations)*0.005, stepfun(a$days, (0:nrow(a)))(time.marks), type="s",lwd=2,
-       xlab="Transformed time", ylab="Cum. Freq.", xaxs="i", yaxs="i",xlim=c(0,5100), ylim=c(0, 5200),main="(b)",
-       cex.main=1.5)
-  abline(0,1,lty=2, lwd=1.5)
-
-  points(0:5089, 5089* qbeta(0.975, 0:5089+1, 5089-(0:5089)+1),type="l", lty=2)
-  points(0:5089, 5089* qbeta(0.025, 0:5089+1, 5089-(0:5089)+1),type="l", lty=2)
-
-dev.off()
+# creating fig4(b)
+p <- ggplot(data.frame(x = cumsum(lambda_at_all_locations) * space_units,
+                  y = stepfun(a$days, 0:nrow(a))(time_marks)),
+       aes(x,y)) + 
+  geom_line(color = "darkgreen") + geom_abline(intercept = 0, slope = 1,color = "black") +
+  geom_ribbon(data = data.frame(x = 0:nrow(a),y = 0,
+                         ymax = nrow(a) * qbeta(.975, 0:nrow(a) + 1, nrow(a) - (0:nrow(a)) + 1),
+                         ymin = nrow(a) * qbeta(.025, 0:nrow(a) + 1, nrow(a) - (0:nrow(a)) + 1)),
+              aes(x, ymin = ymin, ymax = ymax), alpha = .4) +
+  scale_x_continuous(breaks = seq(0, 10000, 2000)) + 
+  scale_y_continuous(breaks = seq(0, 10000, 2000))
+ggsave("figures/trans_time.png", plot = p)
