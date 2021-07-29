aef8a86823abf9fbdddb449ef68b4afcf1c4d56f
Jan Povala <jan.povala@gmail.com>
2021-07-29 15:52:37 +0100

    Adding experiment launcher which works for UNIX systems.

--- experiments_launcher.sh
+++ experiments_launcher.sh
@@ -1,24 +1,21 @@
-# These will be run with trend bandwidth 20.
+# This will run the following combinations
 
-#Rscript run.R --regenerate --numsmooth 2  --follow_trig_prob 0.0 --parents_proportion 1.0 --experimentid "test_homogeneous"
-#Rscript run.R --regenerate --numsmooth 3  --follow_trig_prob 0.0 --parents_proportion 1.0 --experimentid "test_homogeneous"
-#Rscript run.R --regenerate --numsmooth 5  --follow_trig_prob 0.0 --parents_proportion 1.0 --experimentid "test_homogeneous"
-#Rscript run.R --regenerate --numsmooth 10  --follow_trig_prob 0.0 --parents_proportion 1.0 --experimentid "test_homogeneous"
-#Rscript run.R --regenerate --numsmooth 20  --follow_trig_prob 0.0 --parents_proportion 1.0 --experimentid "test_homogeneous"
-#Rscript run.R --regenerate --numsmooth 30  --follow_trig_prob 0.0 --parents_proportion 1.0 --experimentid "test_homogeneous"
-Rscript run.R --regenerate --numsmooth 50  --follow_trig_prob 0.0 --parents_proportion 1.0 --experimentid "test_homogeneous"
-Rscript run.R --regenerate --numsmooth 70  --follow_trig_prob 0.0 --parents_proportion 1.0 --experimentid "test_homogeneous" 
-Rscript run.R --regenerate --numsmooth 100  --follow_trig_prob 0.0 --parents_proportion 1.0 --experimentid "test_homogeneous"
-
-
-
-Rscript run.R --regenerate --numsmooth 2  --follow_trig_prob 0.05 --parents_proportion 0.9 --experimentid "test_clustering"
-Rscript run.R --regenerate --numsmooth 3  --follow_trig_prob 0.05 --parents_proportion 0.9 --experimentid "test_clustering"
-Rscript run.R --regenerate --numsmooth 5  --follow_trig_prob 0.05 --parents_proportion 0.9 --experimentid "test_clustering"
-Rscript run.R --regenerate --numsmooth 10  --follow_trig_prob 0.05 --parents_proportion 0.9 --experimentid "test_clustering"
-Rscript run.R --regenerate --numsmooth 20  --follow_trig_prob 0.05 --parents_proportion 0.9 --experimentid "test_clustering"
-Rscript run.R --regenerate --numsmooth 30  --follow_trig_prob 0.05 --parents_proportion 0.9 --experimentid "test_clustering"
-Rscript run.R --regenerate --numsmooth 50  --follow_trig_prob 0.05 --parents_proportion 0.9 --experimentid "test_clustering"
-Rscript run.R --regenerate --numsmooth 70  --follow_trig_prob 0.05 --parents_proportion 0.9 --experimentid "test_clustering" 
-Rscript run.R --regenerate --numsmooth 100  --follow_trig_prob 0.05 --parents_proportion 0.9 --experimentid "test_clustering" 
-
+for N_P in "2" "5"
+do
+    for BW_H in "0.4" "0.6"
+    do
+        for BW_G  in "1.0" "2.0"
+        do
+            for BW_TREND in "20" "10"
+            do
+                for BW_DAILY in "0.041666667" "0.020833333" "0.083333333" # 1, 0.5, 2 hrs
+                do
+		    for BW_WEEKLY in "0.333333333" "0.166666667" "0.666666667" # 8, 4, 16 hrs
+		    do
+			Rscript run.R --snapshot --numsmooth ${N_P} --bw_daily ${BW_DAILY} --bw_weekly ${BW_WEEKLY} --bw_trend ${BW_TREND} --bw_g ${BW_G} --bw_h ${BW_H} --follow_trig_prob 0.05 --parents_proportion 0.9 --experimentid "test_clustering" 
+		    done
+                done
+            done
+        done
+    done
+done
