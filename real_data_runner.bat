940d5170c6aa987b175f2c518c9116fcda5975bd
Lara Vomfell <l.vomfell@warwick.ac.uk>
2022-02-12 18:03:53 +0000

    added additional param vals

--- real_data_runner.bat
+++ real_data_runner.bat
@@ -5,9 +5,9 @@ SetLocal
 set NP=2 5 10
 set BW_H=0.6 0.8 1.0 1.2
 set BW_G=1.0
-set BW_TREND=10 20
-set BW_WEEKLY=0.33333
-set BW_DAILY=0.041667
+set BW_TREND=5 10 15 20
+set BW_WEEKLY=0.166666 0.33333 0.5
+set BW_DAILY=0.020833 0.041667 0.083333
 
 
 ::Rscript run.R --snapshot --numsmooth %%n --bw_h %%h --bw_g %%g --bw_daily %%d --bw_weekly %%w --bw_trend %%t --real_data --experimentid "real_data_main_run" 
@@ -21,7 +21,7 @@ for %%n in (%NP%) do (
           for %%d in (%BW_DAILY%) do (
     	      echo  %%n %%g %%h %%t %%w %%d
             Rscript run.R --snapshot --numsmooth %%n --bw_h %%h --bw_g %%g --bw_daily %%d --bw_weekly %%w --bw_trend %%t --real_data --experimentid "real_data_main_run"
-            Rscript run.R --snapshot --g_delay --numsmooth %%n --bw_h %%h --bw_g %%g --bw_daily %%d --bw_weekly %%w --bw_trend %%t --real_data --experimentid "real_data_main_run"
+::Rscript run.R --snapshot --g_delay --numsmooth %%n --bw_h %%h --bw_g %%g --bw_daily %%d --bw_weekly %%w --bw_trend %%t --real_data --experimentid "real_data_main_run"
           )
         )
       )
